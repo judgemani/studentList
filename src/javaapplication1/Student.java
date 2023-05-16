82dc31c9b9044175e43b093792b74a0f953daf81
Mani <mankirat6811judge@gmail.com>
2023-05-16 09:59:25 -0700

    Please commit

---
+++ src/javaapplication1/Student.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author manki
+ */
+class Student {
+    private String sname;
+    private String sID;
+
+    public Student(String sname, String sID) {
+        this.sname = sname;
+        this.sID = sID;
+    }
+    
+    public String getSname() {
+        return sname;
+    }
+
+    public void setSname(String sname) {
+        this.sname = sname;
+    }
+
+    public String getsID() {
+        return sID;
+    }
+
+    public void setsID(String sID) {
+        this.sID = sID;
+    }
+}
---
+++ src/javaapplication1/studentList.java
@@ -0,0 +1,19 @@
+package javaapplication1;
+
+
+public class studentList {
+    public static void main(String[]args){
+    Student s1 = new Student("mani","s1");
+    Student[] studentList = new Student[3];
+    studentList[0] = s1;
+    studentList[1] = new Student("abc","s2");
+    studentList[1] = new Student("def","s3");
+
+    for(int i=0; i < studentList.length; i++){
+        System.out.println(studentList[i].getSname());
+    }
+    
+    
+    }
+}
+
